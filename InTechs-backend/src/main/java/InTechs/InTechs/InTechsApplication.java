bda1da0cf64dcc600fc753f6681fff5dd218db4b
서영 <66578746+lliimm318@users.noreply.github.com>
2021-08-19 21:49:23 +0900

    Add files via upload

---
+++ InTechs-backend/src/main/java/InTechs/InTechs/InTechsApplication.java
@@ -0,0 +1,13 @@
+package InTechs.InTechs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InTechsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InTechsApplication.class, args);
+	}
+
+}
---
+++ InTechs-backend/src/test/java/InTechs/InTechs/InTechsApplicationTests.java
@@ -0,0 +1,13 @@
+package InTechs.InTechs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InTechsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

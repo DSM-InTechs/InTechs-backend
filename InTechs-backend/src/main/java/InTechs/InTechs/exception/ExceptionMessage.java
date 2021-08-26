a44dcf93b3e5abae1f3d9d286094f19954aa3ad4
임서영 <sylim0318@naver.com>
2021-08-26 11:48:18 +0900

    [ADD] Invalid token, user vnot found 추가

--- InTechs-backend/src/main/java/InTechs/InTechs/exception/ExceptionMessage.java
+++ InTechs-backend/src/main/java/InTechs/InTechs/exception/ExceptionMessage.java
@@ -4,7 +4,10 @@ import lombok.Getter;
 
 @Getter
 public enum ExceptionMessage {
-    BAD_REQUEST(400, "BAD_REQUEST");
+
+    BAD_REQUEST(400, "BAD_REQUEST"),
+    INVALID_TOKEN(401, "INVALID_TOKEN"),
+    USER_NOT_FOUND(404, "USER_NOT_FOUND");
 
     private final int status;
     private final String message;

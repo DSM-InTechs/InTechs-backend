8d428f5d254b0cbeab1da0e15413b458d1a10505
joungeun <lje0818ovo@naver.com>
2021-08-29 23:28:53 +0900

    [ADD] Test photo upload parameters

--- InTechs-backend/src/main/java/InTechs/InTechs/controller/ProjectController.java
+++ InTechs-backend/src/main/java/InTechs/InTechs/controller/ProjectController.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.validation.Valid;
 
+@RestController
+@Log
 @RequiredArgsConstructor
 @RequestMapping("/project")
 public class ProjectController {
@@ -20,5 +22,17 @@ public class ProjectController {
     public ResponseEntity<String> projectCreate(@RequestHeader("Authorization") String token, @RequestBody @Valid ProjectCreateRequest project){
         projectService.createProject(project, token); // token.getUserId
         return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @RequestMapping("/file")
+    public String uploadFileTest(@RequestPart MultipartFile file){
+        log.info("--진입--");
+        return fileUploadService.uploadImage(file);
+    }
+
+    @RequestMapping("/file02")
+    public String uploadFileTest02(@RequestParam("file") MultipartFile file){
+        log.info("--진입--");
+        return fileUploadService.uploadImage(file);
     }
 }

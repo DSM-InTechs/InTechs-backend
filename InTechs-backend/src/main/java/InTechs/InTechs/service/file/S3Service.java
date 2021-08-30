dcba998270dce0b256aa57c9f0178dec2e84874b
joungeun <lje0818ovo@naver.com>
2021-08-30 23:22:59 +0900

    [ADD] File upload to S3

---
+++ InTechs-backend/src/main/java/InTechs/InTechs/service/file/S3Service.java
@@ -0,0 +1,28 @@
+package InTechs.InTechs.service.file;
+
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.io.InputStream;
+
+@RequiredArgsConstructor
+@Service
+public class S3Service {
+    private final AmazonS3Client amazonS3Client;
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    public void uploadFile(InputStream inputStream, ObjectMetadata objectMetadata, String fileName){
+        amazonS3Client.putObject(new PutObjectRequest(bucket, fileName, inputStream, objectMetadata).withCannedAcl(CannedAccessControlList.PublicRead));
+    }
+
+    public String getFileUrl(String fileName){
+        return String.valueOf(amazonS3Client.getUrl(bucket, fileName));
+    }
+}

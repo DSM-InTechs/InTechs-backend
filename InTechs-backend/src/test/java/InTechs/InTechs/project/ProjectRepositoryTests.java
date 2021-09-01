6ad14f0cb0136bdd0b1c9393b58c1ea0a5e330ad
joungeun <lje0818ovo@naver.com>
2021-09-01 19:41:28 +0900

    [TEST] Project image update query

--- InTechs-backend/src/test/java/InTechs/InTechs/project/ProjectRepositoryTests.java
+++ InTechs-backend/src/test/java/InTechs/InTechs/project/ProjectRepositoryTests.java
@@ -1,5 +1,6 @@
 package InTechs.InTechs.project;
 
+import InTechs.InTechs.entity.Image;
 import InTechs.InTechs.repository.project.CustomProjectRepository;
 import InTechs.InTechs.repository.project.ProjectRepository;
 import org.junit.jupiter.api.Test;
@@ -12,9 +13,18 @@ public class ProjectRepositoryTests {
     ProjectRepository projectRepository;
 
     @Test
-    public void nameChangeMethod(){
+    public void nameChangeTest(){
         int number = 297102;
         String name = "변경된 이름";
         projectRepository.projectUpdate(number, name);
+    }
+    @Test
+    public void ImageChangeTest(){
+        int number = 297102;
+        Image image = Image.builder()
+                .imageUrl("어쨌든 url임")
+                .oriName("어쨌든.jpg")
+                .build();
+        projectRepository.projectUpdate(number, image);
     }
 }

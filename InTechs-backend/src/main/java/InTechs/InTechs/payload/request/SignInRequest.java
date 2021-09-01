87e7d5ef57cdb2cbd122053afef940ea8b6166df
임서영 <sylim0318@naver.com>
2021-09-01 16:08:05 +0900

    [ADD] sign in request

---
+++ InTechs-backend/src/main/java/InTechs/InTechs/payload/request/SignInRequest.java
@@ -0,0 +1,15 @@
+package InTechs.InTechs.payload.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class SignInRequest {
+
+    private String email;
+    private String password;
+
+}

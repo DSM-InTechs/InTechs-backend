813bb628e23e7ea8ceafcf55f029404ce18d04c2
임서영 <sylim0318@naver.com>
2021-09-01 16:02:49 +0900

    [ADD] token request

---
+++ InTechs-backend/src/main/java/InTechs/InTechs/payload/request/TokenRequest.java
@@ -0,0 +1,14 @@
+package InTechs.InTechs.payload.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TokenRequest {
+
+    private String refreshToken;
+
+}

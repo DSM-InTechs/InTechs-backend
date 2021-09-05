f50877491b5b6117cd534e76ef2408a50a3bb2e3
joungeun <lje0818ovo@naver.com>
2021-09-05 18:39:38 +0900

    [TEST] ProjectJoinService user update test

--- InTechs-backend/src/test/java/InTechs/InTechs/project/ProjectRepositoryTests.java
+++ InTechs-backend/src/test/java/InTechs/InTechs/project/ProjectRepositoryTests.java
@@ -2,16 +2,22 @@ package InTechs.InTechs.project;
 
 import InTechs.InTechs.entity.Image;
 import InTechs.InTechs.entity.Project;
+import InTechs.InTechs.entity.User;
 import InTechs.InTechs.exception.exceptions.ProjectNotFoundException;
 import InTechs.InTechs.repository.project.ProjectRepository;
+import InTechs.InTechs.repository.user.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootTest
 public class ProjectRepositoryTests {
     @Autowired
     ProjectRepository projectRepository;
+    @Autowired
+    UserRepository userRepository;
 
     @Test
     public void nameChangeTest(){
@@ -39,5 +45,26 @@ public class ProjectRepositoryTests {
     public void test(){
         Project project = projectRepository.findById(297102).orElseThrow(ProjectNotFoundException::new);
         System.out.println(project.getUsers().get(0).getEmail());
+    }
+
+    @Test
+    public void test02(){
+        List<User> users = new ArrayList<>();
+        users.add(userRepository.findById("whddms@dsm.hs.kr").get());
+        users.add(userRepository.findById("ddd@com").get());
+        Project project = Project.builder()
+                .number(879465)
+                .name("proName")
+                .image(Image.builder().build())
+                .users(users).build();
+
+        projectRepository.save(project);
+    }
+
+    @Test
+    public void userUpdateTest(){
+        Project project = projectRepository.findById(879465).orElseThrow(ProjectNotFoundException::new);
+        project.addUser(userRepository.findById("ddd03@com").get());
+        projectRepository.save(project);
     }
 }

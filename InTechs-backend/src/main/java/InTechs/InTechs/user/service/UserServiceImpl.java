541026c1f58ad85555624f078f747b4b6e863722
임서영 <sylim0318@naver.com>
2021-09-15 07:33:20 +0900

    [FIX] 이미지 업로드

--- InTechs-backend/src/main/java/InTechs/InTechs/user/service/UserServiceImpl.java
+++ InTechs-backend/src/main/java/InTechs/InTechs/user/service/UserServiceImpl.java
@@ -1,9 +1,12 @@
 package InTechs.InTechs.user.service;
 
+import InTechs.InTechs.exception.exceptions.ProjectNotFoundException;
+import InTechs.InTechs.user.entity.Project;
 import InTechs.InTechs.user.entity.User;
 import InTechs.InTechs.exception.exceptions.UserAlreadyException;
 import InTechs.InTechs.exception.exceptions.UserNotFoundException;
 import InTechs.InTechs.user.payload.request.ProfileRequest;
+import InTechs.InTechs.user.payload.response.IsActiveResponse;
 import InTechs.InTechs.user.payload.response.MyPageResponse;
 import InTechs.InTechs.user.payload.response.MyProjectListResponse;
 import InTechs.InTechs.user.payload.response.ProfileResponse;
@@ -11,17 +14,23 @@ import InTechs.InTechs.user.repository.ProjectRepository;
 import InTechs.InTechs.user.repository.UserRepository;
 import InTechs.InTechs.user.security.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
 
+    @Value("${image.upload.dir}")
+    private String imageDir;
+
     private final UserRepository userRepository;
     private final ProjectRepository projectRepository;
+
     private final JwtTokenProvider jwtTokenProvider;
 
     @Override
@@ -42,10 +51,14 @@ public class UserServiceImpl implements UserService {
         User user = userRepository.findByEmail(jwtTokenProvider.getEmail(token))
                 .orElseThrow(UserAlreadyException::new);
 
+        String fileName = user.getEmail();
+
+        File file = new File(imageDir, fileName);
+
         return MyPageResponse.builder()
                 .name(user.getName())
                 .email(user.getEmail())
-                .image(user.getImage())
+                .image(fileName)
                 .build();
     }
 
@@ -60,12 +73,17 @@ public class UserServiceImpl implements UserService {
 
         userRepository.save(user.setName(name));
 
-        userRepository.save(user.setImage(image));
+        userRepository.save(user.setImage(image.toString()));
     }
 
     @Override
     public List<MyProjectListResponse> getMyProject(String token) {
 
+        return null;
+    }
+
+    @Override
+    public IsActiveResponse getActive(Boolean isActive) {
         return null;
     }
 

d7bd3dd6efb35ff35dcb5419f80a692256360271
joungeun <lje0818ovo@naver.com>
2021-09-19 06:06:37 +0900

    [TEST] Issue Filtering test

--- InTechs-backend/src/main/java/InTechs/InTechs/issue/repository/IssueRepository.java
+++ InTechs-backend/src/main/java/InTechs/InTechs/issue/repository/IssueRepository.java
@@ -2,6 +2,12 @@ package InTechs.InTechs.issue.repository;
 
 import InTechs.InTechs.issue.entity.Issue;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.List;
 
 public interface IssueRepository extends MongoRepository<Issue, String> {
+    @Query("{projectId:?0, userIds:{$in:?1}")
+    List<Issue> findProjectId(int projectId, List<String> userId);
+    List<Issue> findAllByProjectId(int projectId);
 }
--- InTechs-backend/src/test/java/InTechs/InTechs/issue/IssueRepositoryTests.java
+++ InTechs-backend/src/test/java/InTechs/InTechs/issue/IssueRepositoryTests.java
@@ -3,9 +3,15 @@ package InTechs.InTechs.issue;
 import InTechs.InTechs.issue.entity.Issue;
 import InTechs.InTechs.issue.repository.IssueRepository;
 import InTechs.InTechs.issue.value.State;
+import InTechs.InTechs.issue.value.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 
 @SpringBootTest
@@ -19,5 +25,15 @@ public class IssueRepositoryTests {
         issue.setContent("그러지마");
         issue.setState(State.IN_PROGRESS);
         issueRepository.save(issue);
+    }
+
+    @Test
+    public void issueFiltering(){
+        int projectId = 879487;
+        List<String> users = new ArrayList<>();
+        Set<Tag> tags = new HashSet<>();
+        tags.add(Tag.builder().tag("백엔드").build());
+        users.add("whddms");
+        issueRepository.findProjectId(projectId, users);
     }
 }

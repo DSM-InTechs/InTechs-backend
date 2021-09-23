7e1d13deb23c9619f5b25345d0880bcdbc531d96
임서영 <sylim0318@naver.com>
2021-09-23 11:30:01 +0900

    [FIX] 파일 구조 변경

--- InTechs-backend/src/test/java/InTechs/InTechs/mongoDB/MongoDBTests.java
+++ InTechs-backend/src/test/java/InTechs/InTechs/mongoDB/MongoDBTests.java
@@ -1,7 +1,7 @@
 package InTechs.InTechs.mongoDB;
 
-import InTechs.InTechs.user.entity.Project;
-import InTechs.InTechs.user.repository.ProjectRepository;
+import InTechs.InTechs.project.entity.Project;
+import InTechs.InTechs.project.repository.ProjectRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;

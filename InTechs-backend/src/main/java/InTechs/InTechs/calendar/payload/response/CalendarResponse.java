d432436b650d2ab686e64bee448ebd112baa420b
임서영 <sylim0318@naver.com>
2021-09-27 11:04:58 +0900

    [ADD] calendar response

---
+++ InTechs-backend/src/main/java/InTechs/InTechs/calendar/payload/response/CalendarResponse.java
@@ -0,0 +1,20 @@
+package InTechs.InTechs.calendar.payload.response;
+
+import InTechs.InTechs.issue.value.State;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import org.bson.types.ObjectId;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class CalendarResponse {
+
+    private final String name;
+    private final int progress;
+    private final State state;
+    private final String date;
+    private final String content;
+
+}

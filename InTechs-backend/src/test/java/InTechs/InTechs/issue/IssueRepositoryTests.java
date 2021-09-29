0b8ae9fe12ae577190acc65c73f4ef7e401b9b9a
joungeun <lje0818ovo@naver.com>
2021-09-29 22:38:36 +0900

    [TEST] IssueCountTests

--- InTechs-backend/src/test/java/InTechs/InTechs/issue/IssueRepositoryTests.java
+++ InTechs-backend/src/test/java/InTechs/InTechs/issue/IssueRepositoryTests.java
@@ -1,9 +1,11 @@
 package InTechs.InTechs.issue;
 
+import InTechs.InTechs.exception.exceptions.UserNotFoundException;
 import InTechs.InTechs.issue.entity.Issue;
 import InTechs.InTechs.issue.repository.IssueRepository;
 import InTechs.InTechs.issue.value.State;
 import InTechs.InTechs.issue.value.Tag;
+import InTechs.InTechs.user.repository.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,11 +15,16 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import static InTechs.InTechs.issue.value.State.DONE;
+import static org.assertj.core.api.Assertions.*;
+
 
 @SpringBootTest
 public class IssueRepositoryTests {
     @Autowired
     IssueRepository issueRepository;
+    @Autowired
+    UserRepository userRepository;
 
     @Test
     public void issueUpdate(){
@@ -35,5 +42,22 @@ public class IssueRepositoryTests {
         tags.add(Tag.builder().tag("백엔드").build());
         users.add("whddms");
         issueRepository.findProjectId(projectId, users);
+    }
+
+    @Test
+    public void issueStateCountTest(){
+        long c = issueRepository.countByStateAndProjectId(DONE, 879487);
+        assertThat(c).isEqualTo(2);
+    }
+
+    @Test
+    public void myIssueCountTest(){
+        int projectId = 879487;
+        String userId = "whddms@dsm.hs.kr";
+        long myIssueCount = issueRepository.findAllByProjectId(projectId).stream().filter(
+                (a)->a.getUsers().contains(
+                        userRepository.findById(userId).orElseThrow(UserNotFoundException::new)
+                )).count();
+        assertThat(myIssueCount).isEqualTo(2);
     }
 }

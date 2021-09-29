9e0db7b29ca6243fe56e99ae7e68d72cc57e2a06
joungeun <lje0818ovo@naver.com>
2021-09-29 22:44:04 +0900

    [ADD] Issue count method (countByStateAndProjectId)

--- InTechs-backend/src/main/java/InTechs/InTechs/issue/repository/IssueRepository.java
+++ InTechs-backend/src/main/java/InTechs/InTechs/issue/repository/IssueRepository.java
@@ -1,6 +1,7 @@
 package InTechs.InTechs.issue.repository;
 
 import InTechs.InTechs.issue.entity.Issue;
+import InTechs.InTechs.issue.value.State;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 
@@ -10,4 +11,5 @@ public interface IssueRepository extends MongoRepository<Issue, String> {
     @Query("{projectId:?0, userIds:{$in:?1}")
     List<Issue> findProjectId(int projectId, List<String> userId);
     List<Issue> findAllByProjectId(int projectId);
+    Long countByStateAndProjectId(State state, int projectId);
 }

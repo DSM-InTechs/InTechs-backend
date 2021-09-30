dbf29077aeeeea547e0aad289836558a7b62b60e
임서영 <sylim0318@naver.com>
2021-09-30 23:18:50 +0900

    [FIX] request로 변경

--- InTechs-backend/src/main/java/InTechs/InTechs/calendar/service/CalendarService.java
+++ InTechs-backend/src/main/java/InTechs/InTechs/calendar/service/CalendarService.java
@@ -1,14 +1,17 @@
 package InTechs.InTechs.calendar.service;
 
-import InTechs.InTechs.calendar.payload.response.CalendarFilterResponse;
+import InTechs.InTechs.calendar.payload.request.FilterRequest;
 import InTechs.InTechs.calendar.payload.response.CalendarResponse;
+import InTechs.InTechs.issue.value.State;
+import InTechs.InTechs.issue.value.Tag;
 
 import java.util.List;
+import java.util.Set;
 
 public interface CalendarService {
 
-    List<CalendarResponse> getCalendar(String issueId);
+    List<CalendarResponse> getCalendar(int projectId, int year, int month);
 
-    //List<CalendarFilterResponse> getFilterCalendar();
+    List<CalendarResponse> getFilterCalendar(int projectId, FilterRequest filterRequest);
 
 }
--- InTechs-backend/src/main/java/InTechs/InTechs/calendar/service/CalendarServiceImpl.java
+++ InTechs-backend/src/main/java/InTechs/InTechs/calendar/service/CalendarServiceImpl.java
@@ -1,33 +1,58 @@
 package InTechs.InTechs.calendar.service;
 
+import InTechs.InTechs.calendar.payload.request.FilterRequest;
 import InTechs.InTechs.calendar.payload.response.CalendarResponse;
-import InTechs.InTechs.calendar.repository.CalendarRepository;
-import InTechs.InTechs.exception.exceptions.ProjectNotFoundException;
+import InTechs.InTechs.issue.entity.Issue;
 import InTechs.InTechs.issue.repository.IssueRepository;
+import InTechs.InTechs.issue.value.State;
+import InTechs.InTechs.issue.value.Tag;
 import lombok.RequiredArgsConstructor;
-import org.bson.types.ObjectId;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class CalendarServiceImpl implements CalendarService {
 
-    private IssueRepository issueRepository;
+    private final IssueRepository issueRepository;
 
     @Override
-    public List<CalendarResponse> getCalendar(String issueId) {
-        return issueRepository.findById(issueId).stream()
-                .map(issue -> CalendarResponse.builder()
-                    .name(issue.getWriter())
-                    .content(issue.getContent())
-                    .date(issue.getEnd_date())
-                    .state(issue.getState())
-                    .progress(issue.getProgress())
-                    .build())
-                .collect(Collectors.toList());
+    public List<CalendarResponse> getCalendar(int projectId, int year, int month) {
+        String monthString = String.valueOf(month);
+        if (month < 10) {
+            monthString = "0" + monthString;
+        }
+
+        List<Issue> issues = issueRepository.findByProjectIdAndEndDateStartingWith(projectId, year + monthString);
+
+        return buildCalendar(issues);
     }
 
+    @Override
+    public List<CalendarResponse> getFilterCalendar(int projectId, FilterRequest filterRequest) {
+
+        Set<Tag> tags = filterRequest.getTags();
+        State state = filterRequest.getState();
+        String name = filterRequest.getName();
+
+        List<Issue> issues = issueRepository.findByProjectIdAndTag(projectId, name, state, tags);
+
+        return buildCalendar(issues);
+    }
+
+    private List<CalendarResponse> buildCalendar(List<Issue> issues) {
+        return issues.stream()
+                .map(issue -> CalendarResponse.builder()
+                        .id(issue.getId().toString())
+                        .name(issue.getWriter())
+                        .state(issue.getState())
+                        .progress(issue.getProgress())
+                        .content(issue.getContent())
+                        .date(issue.getEndDate())
+                        .build())
+                .collect(Collectors.toList());
+    }
 }

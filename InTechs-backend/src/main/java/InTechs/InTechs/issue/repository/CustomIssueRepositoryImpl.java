a01fed72638548d7b2304a0aef108bc2c3c4f614
임서영 <sylim0318@naver.com>
2021-10-01 18:49:22 +0900

    [ADD] CustomIsuue Repository

---
+++ InTechs-backend/src/main/java/InTechs/InTechs/issue/repository/CustomIssueRepositoryImpl.java
@@ -0,0 +1,37 @@
+package InTechs.InTechs.issue.repository;
+
+import InTechs.InTechs.issue.entity.Issue;
+import InTechs.InTechs.issue.value.State;
+import InTechs.InTechs.issue.value.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Set;
+
+import static org.springframework.data.mongodb.core.query.Criteria.where;
+import static org.springframework.data.mongodb.core.query.Query.query;
+
+@RequiredArgsConstructor
+@Repository
+public class CustomIssueRepositoryImpl implements CustomIssueRepository {
+
+    private final MongoTemplate mongoTemplate;
+
+    public List<Issue> findProjectAndEndDate(int projectId, String date) {
+        return mongoTemplate.find(query(where("projectId").is(projectId)
+                .and("endDate").regex(date)),
+                Issue.class);
+    }
+
+    public List<Issue> findByProjectIdAndTag(int projectId, String writer, State state, Set<Tag> tags) {
+        return mongoTemplate.find(query(where("projectId").is(projectId)
+                .and("tags.tag").in(tags)
+                .orOperator(where("writer").is(writer),
+                        (where("state")).is(state))),
+                Issue.class);
+    }
+
+
+}

961c88eae9d5aca253585a42de2fe3bd8b527a1e
joungeun <lje0818ovo@naver.com>
2021-10-05 11:24:05 +0900

    [ADD] @Getter to DashboardResponse

--- InTechs-backend/src/main/java/InTechs/InTechs/project/value/IssuesCountInfo.java
+++ InTechs-backend/src/main/java/InTechs/InTechs/project/value/IssuesCountInfo.java
@@ -1,13 +1,15 @@
 package InTechs.InTechs.project.value;
 
 import lombok.Builder;
+import lombok.Getter;
 import lombok.ToString;
 
+@Getter
 @ToString
 @Builder
 public class IssuesCountInfo {
     long forMe;
     long resolved;
     long unresolved;
-    long foreMeAndUnresolved;
+    long forMeAndUnresolved;
 }

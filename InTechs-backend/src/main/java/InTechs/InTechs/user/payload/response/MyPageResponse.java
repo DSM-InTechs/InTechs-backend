3d198539fbea1cfe5e7cd1192fcb11407d7e4670
임서영 <sylim0318@naver.com>
2021-10-25 19:02:43 +0900

    [FIX] 필요없는 import문 삭제!

--- InTechs-backend/src/main/java/InTechs/InTechs/user/payload/response/MyPageResponse.java
+++ InTechs-backend/src/main/java/InTechs/InTechs/user/payload/response/MyPageResponse.java
@@ -3,7 +3,6 @@ package InTechs.InTechs.user.payload.response;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import org.springframework.web.multipart.MultipartFile;
 
 @Getter
 @Builder

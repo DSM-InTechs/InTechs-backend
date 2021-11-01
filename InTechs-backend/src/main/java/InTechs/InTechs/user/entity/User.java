4d765fe3cf9e59e372fe7434274a0675ffef2ae6
임서영 <sylim0318@naver.com>
2021-11-01 18:54:40 +0900

    [FIX] @DATA로 묶음

--- InTechs-backend/src/main/java/InTechs/InTechs/user/entity/User.java
+++ InTechs-backend/src/main/java/InTechs/InTechs/user/entity/User.java
@@ -1,5 +1,6 @@
 package InTechs.InTechs.user.entity;
 
+import InTechs.InTechs.chat.entity.Channel;
 import InTechs.InTechs.project.entity.Project;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -16,12 +17,9 @@ import java.util.List;
 
 import lombok.*;
 
-@EqualsAndHashCode
-@Document(collection = "user")
-@Getter
+@Data
 @Builder
-@NoArgsConstructor
-@AllArgsConstructor
+@Document(collection = "user")
 public class User {
 
     @Id
@@ -41,6 +39,9 @@ public class User {
 
     @DBRef(lazy = true)
     private List<Project> project;
+
+    @DBRef(lazy = true)
+    private List<Channel> channels;
 
     public User updateActive(boolean isActive) {
         this.isActive = isActive;

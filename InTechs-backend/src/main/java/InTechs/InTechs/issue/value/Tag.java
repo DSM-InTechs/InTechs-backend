5432ec248a09706f1a7080f7aa0fe29166ad79be
임서영 <sylim0318@naver.com>
2021-11-03 23:44:29 +0900

    [ADD] valueOf

--- InTechs-backend/src/main/java/InTechs/InTechs/issue/value/Tag.java
+++ InTechs-backend/src/main/java/InTechs/InTechs/issue/value/Tag.java
@@ -6,6 +6,11 @@ import lombok.*;
 @Getter
 @EqualsAndHashCode
 @Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class Tag {
     private String tag;
+
+    public static Tag valueOf(String tag) {
+        return new Tag(tag);
+    }
 }

ab998f3a1fcc096a95452dc812d1f81dd680f38e
임서영 <sylim0318@naver.com>
2021-11-08 20:13:35 +0900

    [FIX] mapping fix

--- InTechs-backend/src/main/java/InTechs/InTechs/user/entity/User.java
+++ InTechs-backend/src/main/java/InTechs/InTechs/user/entity/User.java
@@ -1,6 +1,7 @@
 package InTechs.InTechs.user.entity;
 
 import InTechs.InTechs.chat.entity.Channel;
+import InTechs.InTechs.chat.entity.Chat;
 import InTechs.InTechs.project.entity.Project;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -42,6 +43,9 @@ public class User {
 
     @DBRef(lazy = true)
     private List<Channel> channels;
+
+    @DBRef(lazy = true)
+    private List<Chat> Chats;
 
     public User updateActive(boolean isActive) {
         this.isActive = isActive;

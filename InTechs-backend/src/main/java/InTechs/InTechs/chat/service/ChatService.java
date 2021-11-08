8a88fd33fe4df6f7eb1c702c8fc91c0abb2e468c
임서영 <sylim0318@naver.com>
2021-11-08 20:24:21 +0900

    [ADD] chat service

--- InTechs-backend/src/main/java/InTechs/InTechs/chat/service/ChatService.java
+++ InTechs-backend/src/main/java/InTechs/InTechs/chat/service/ChatService.java
@@ -1,11 +1,18 @@
 package InTechs.InTechs.chat.service;
 
+import InTechs.InTechs.chat.payload.request.ChannelRequest;
+import InTechs.InTechs.chat.payload.request.NoticeRequest;
 import InTechs.InTechs.chat.payload.response.ChatResponse;
+import InTechs.InTechs.user.entity.User;
 
 import java.util.List;
 
 public interface ChatService {
 
-    List<ChatResponse> get
+    List<ChatResponse> getChatList(String channelId);
+
+    void sendChat(User user, String channelId, String message);
+
+    void updateChannel(String channelId, ChannelRequest channelRequest);
 
 }

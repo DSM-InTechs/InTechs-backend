a9dedec74fbe2ce6c2eafcd72a2b547c7fcede44
임서영 <sylim0318@naver.com>
2021-11-08 20:13:05 +0900

    [FIX] mapping fix

--- InTechs-backend/src/main/java/InTechs/InTechs/chat/entity/Chat.java
+++ InTechs-backend/src/main/java/InTechs/InTechs/chat/entity/Chat.java
@@ -1,11 +1,14 @@
 package InTechs.InTechs.chat.entity;
 
+import InTechs.InTechs.user.entity.User;
 import lombok.Builder;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Data
 @Builder
@@ -15,12 +18,24 @@ public class Chat {
     @Id
     private int id;
 
-    private final int channelId;
-
     private String message;
 
     private LocalDateTime time;
 
     private String sender;
+
+    private boolean notice = false;
+
+    @DBRef(lazy = true)
+    private List<User> users;
+
+    private String channelId;
+
+    private int projectId;
+
+    public Chat updateNotice(boolean notice) {
+        this.notice = notice;
+        return this;
+    }
 
 }

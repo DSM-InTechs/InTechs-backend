ccbd7df10610d13db02315329515692278108ccd
임서영 <sylim0318@naver.com>
2021-11-08 20:23:29 +0900

    [FIX] value 값 변경

--- InTechs-backend/src/main/java/InTechs/InTechs/chat/repository/ChannelRepository.java
+++ InTechs-backend/src/main/java/InTechs/InTechs/chat/repository/ChannelRepository.java
@@ -2,16 +2,16 @@ package InTechs.InTechs.chat.repository;
 
 import InTechs.InTechs.chat.entity.Channel;
 import InTechs.InTechs.user.entity.User;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface ChannelRepository extends MongoRepository<Channel, Integer> {
+public interface ChannelRepository extends MongoRepository<Channel, String> {
 
-    boolean existsByChannelIdAndUsers(int channelId, User user);
-
-    Optional<Channel> findByChannelIdAndUsersNot(int channelId, User user);
+    Boolean existsByChannelIdAndUsers(String channelId, User user);
 
 }

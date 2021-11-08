e26a5f0485b7e95247f44e7ba29e62ad61d714a3
임서영 <sylim0318@naver.com>
2021-11-08 20:24:02 +0900

    [UPDATE] Chat repository

--- InTechs-backend/src/main/java/InTechs/InTechs/chat/repository/ChatRepository.java
+++ InTechs-backend/src/main/java/InTechs/InTechs/chat/repository/ChatRepository.java
@@ -1,10 +1,19 @@
 package InTechs.InTechs.chat.repository;
 
 import InTechs.InTechs.chat.entity.Chat;
+import InTechs.InTechs.user.entity.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ChatRepository extends MongoRepository<Chat, Integer> {
+
+    List<Chat> findBySenderAndChannelId(String sender, String channelId);
+
+    List<Chat> findByChannelIdAndNoticeIsTrueOrderByTime(String channelId);
+
+
 
 }

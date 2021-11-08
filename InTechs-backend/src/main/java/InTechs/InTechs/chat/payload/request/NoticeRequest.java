b0d1baa058b252fed7a3c24c7e9bb8ec93a0b1db
임서영 <sylim0318@naver.com>
2021-11-08 20:16:57 +0900

    [ADD] notice request

---
+++ InTechs-backend/src/main/java/InTechs/InTechs/chat/payload/request/NoticeRequest.java
@@ -0,0 +1,12 @@
+package InTechs.InTechs.chat.payload.request;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class NoticeRequest {
+
+    private final boolean notice;
+
+}

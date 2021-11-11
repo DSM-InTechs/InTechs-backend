757e25070d31f4de052b4130895fa9e8d3e49279
임서영 <sylim0318@naver.com>
2021-11-11 20:07:14 +0900

    [FIX] 작동 되는 것만

--- InTechs-backend/src/main/java/InTechs/InTechs/chat/service/NoticeService.java
+++ InTechs-backend/src/main/java/InTechs/InTechs/chat/service/NoticeService.java
@@ -2,17 +2,12 @@ package InTechs.InTechs.chat.service;
 
 import InTechs.InTechs.chat.payload.request.NoticeRequest;
 import InTechs.InTechs.chat.payload.response.ChatResponse;
+import org.bson.types.ObjectId;
 
 import java.util.List;
 
 public interface NoticeService {
 
-    void updateNotice(int chatId, NoticeRequest noticeRequest);
-
-    List<ChatResponse> noticeList(String channelId);
-
-    ChatResponse currentNotice(String channelId);
-
-    ChatResponse detailNotice(int chatId);
+    void updateNotice(String chatId, NoticeRequest noticeRequest);
 
 }
--- InTechs-backend/src/main/java/InTechs/InTechs/chat/service/NoticeServiceImpl.java
+++ InTechs-backend/src/main/java/InTechs/InTechs/chat/service/NoticeServiceImpl.java
@@ -5,12 +5,8 @@ import InTechs.InTechs.chat.payload.request.NoticeRequest;
 import InTechs.InTechs.chat.payload.response.ChatResponse;
 import InTechs.InTechs.chat.repository.ChatRepository;
 import InTechs.InTechs.exception.exceptions.ChatChannelNotFoundException;
-import InTechs.InTechs.exception.exceptions.UserNotFoundException;
-import InTechs.InTechs.file.FileUploader;
-import InTechs.InTechs.security.auth.AuthenticationFacade;
-import InTechs.InTechs.user.entity.User;
-import InTechs.InTechs.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.bson.types.ObjectId;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -20,47 +16,16 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class NoticeServiceImpl implements NoticeService {
 
-    private ChatRepository chatRepository;
+    private final ChatRepository chatRepository;
 
     @Override
-    public void updateNotice(int chatId, NoticeRequest noticeRequest) {
-        Chat chat = chatRepository.findById(chatId)
+    public void updateNotice(String chatId, NoticeRequest noticeRequest) {
+        Chat chat = chatRepository.findById(new ObjectId(chatId))
                 .orElseThrow(ChatChannelNotFoundException::new);
 
-        chatRepository.save(chat.updateNotice(noticeRequest.isNotice()));
-    }
+        Boolean notice = noticeRequest.getNotice();
 
-    @Override
-    public List<ChatResponse> noticeList(String channelId) {
-        return chatRepository.findByChannelIdAndNoticeIsTrueOrderByTime(channelId).stream()
-                .map(chat -> ChatResponse.builder()
-                        .sender(chat.getSender())
-                        .message(chat.getMessage())
-                        .time(chat.getTime())
-                        .build())
-                .collect(Collectors.toList());
-    }
-
-    @Override
-    public ChatResponse currentNotice(String channelId) {
-        List<Chat> notices = chatRepository.findByChannelIdAndNoticeIsTrueOrderByTime(channelId);
-
-        Chat current = chatRepository.findById(notices.get(0).getId())
-                .orElseThrow(ChatChannelNotFoundException::new);
-
-        return ChatResponse.builder()
-                .message(current.getMessage())
-                .sender(current.getSender())
-                .time(current.getTime())
-                .build();
-    }
-
-    @Override
-    public ChatResponse detailNotice(int chatId) {
-        Chat chat = chatRepository.findById(chatId)
-                .orElseThrow(ChatChannelNotFoundException::new);
-
-        return null;
+        chatRepository.save(chat.updateNotice(notice));
     }
 
 }

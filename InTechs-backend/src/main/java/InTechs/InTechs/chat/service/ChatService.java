221a234f36399e34aacc316cbfa30fced1eb3e74
임서영 <sylim0318@naver.com>
2021-11-11 20:06:44 +0900

    [FIX]

--- InTechs-backend/src/main/java/InTechs/InTechs/chat/service/ChatService.java
+++ InTechs-backend/src/main/java/InTechs/InTechs/chat/service/ChatService.java
@@ -1,7 +1,5 @@
 package InTechs.InTechs.chat.service;
 
-import InTechs.InTechs.chat.payload.request.ChannelRequest;
-import InTechs.InTechs.chat.payload.request.NoticeRequest;
 import InTechs.InTechs.chat.payload.response.ChatResponse;
 import InTechs.InTechs.user.entity.User;
 
@@ -12,7 +10,5 @@ public interface ChatService {
     List<ChatResponse> getChatList(String channelId);
 
     void sendChat(User user, String channelId, String message);
-
-    void updateChannel(String channelId, ChannelRequest channelRequest);
 
 }
--- InTechs-backend/src/main/java/InTechs/InTechs/chat/service/ChatServiceImpl.java
+++ InTechs-backend/src/main/java/InTechs/InTechs/chat/service/ChatServiceImpl.java
@@ -1,15 +1,12 @@
 package InTechs.InTechs.chat.service;
 
-import InTechs.InTechs.chat.entity.Channel;
 import InTechs.InTechs.chat.entity.Chat;
-import InTechs.InTechs.chat.payload.request.ChannelRequest;
 import InTechs.InTechs.chat.payload.response.ChatResponse;
 import InTechs.InTechs.chat.repository.ChannelRepository;
 import InTechs.InTechs.chat.repository.ChatRepository;
 import InTechs.InTechs.exception.exceptions.ChatChannelNotFoundException;
 import InTechs.InTechs.exception.exceptions.UserNotFoundException;
 import InTechs.InTechs.file.FileUploader;
-import InTechs.InTechs.file.S3FileUploader;
 import InTechs.InTechs.security.auth.AuthenticationFacade;
 import InTechs.InTechs.user.entity.User;
 import InTechs.InTechs.user.repository.UserRepository;
@@ -59,7 +56,7 @@ public class ChatServiceImpl implements ChatService {
     @SneakyThrows
     @Override
     public void sendChat(User user, String channelId, String message) {
-        boolean existsChannel = channelRepository.existsByChannelIdAndUsers(channelId, user);
+        boolean existsChannel = channelRepository.existsByChannelIdAndUsersContaining(channelId, user);
 
         if(!existsChannel) throw new ChatChannelNotFoundException();
 
@@ -70,14 +67,6 @@ public class ChatServiceImpl implements ChatService {
                         .channelId(channelId)
                 .build()
         );
-    }
-
-    @Override
-    public void updateChannel(String channelId, ChannelRequest channelRequest) {
-        Channel channel = channelRepository.findById(channelId)
-                .orElseThrow(ChatChannelNotFoundException::new);
-
-        channelRepository.save(channel.updateName(channelRequest.getNewName()));
     }
 
     private User findUser() {

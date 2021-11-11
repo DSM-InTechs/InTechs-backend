32d6b4a86a007825a6b743307cb8bd2a41b65521
임서영 <sylim0318@naver.com>
2021-11-11 20:06:27 +0900

    [ADD]

---
+++ InTechs-backend/src/main/java/InTechs/InTechs/chat/service/ChannelService.java
@@ -0,0 +1,22 @@
+package InTechs.InTechs.chat.service;
+
+import InTechs.InTechs.chat.payload.request.ChannelRequest;
+import InTechs.InTechs.user.payload.response.ProfileResponse;
+
+import java.util.List;
+
+public interface ChannelService {
+
+    void updateChannel(String channelId, ChannelRequest channelRequest);
+
+    void createChannel(int projectId, ChannelRequest channelRequest);
+
+    void deleteChannel(String channelId);
+
+    List<ProfileResponse> getProfiles(String channelId);
+
+    void AddUser(String targetEmail, String channelId);
+
+    void exitChannel(String channelId);
+
+}

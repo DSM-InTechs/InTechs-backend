f1e1406ecdd682fec417d2b86a2b38f306512c3b
joungeun <lje0818ovo@naver.com>
2021-11-15 22:12:12 +0900

    [ADD] Repository

--- InTechs-backend/src/main/java/InTechs/InTechs/channel/repository/ChannelRepository.java
+++ InTechs-backend/src/main/java/InTechs/InTechs/channel/repository/ChannelRepository.java
@@ -1,17 +1,11 @@
-package InTechs.InTechs.chat.repository;
+package InTechs.InTechs.channel.repository;
 
-import InTechs.InTechs.chat.entity.Channel;
+import InTechs.InTechs.channel.entity.Channel;
 import InTechs.InTechs.user.entity.User;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Optional;
-
 @Repository
 public interface ChannelRepository extends MongoRepository<Channel, String> {
-
     Boolean existsByChannelIdAndUsersContaining(String channelId, User user);
-
 }

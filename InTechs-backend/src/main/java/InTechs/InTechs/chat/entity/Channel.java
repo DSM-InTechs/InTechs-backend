b27579fcb9c711bf9bd437183401310e2d6c662c
joungeun <lje0818ovo@naver.com>
2021-11-16 22:02:20 +0900

    [ADD] @Data ChannelUser

--- InTechs-backend/src/main/java/InTechs/InTechs/chat/entity/Channel.java
+++ InTechs-backend/src/main/java/InTechs/InTechs/chat/entity/Channel.java
@@ -1,13 +1,12 @@
 package InTechs.InTechs.chat.entity;
 
-import InTechs.InTechs.user.entity.User;
+import InTechs.InTechs.user.entity.ChannelUser;
 import lombok.Builder;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Data
@@ -20,8 +19,7 @@ public class Channel {
 
     private String name;
 
-    @DBRef(lazy = true)
-    private List<User> users;
+    private List<ChannelUser> users;
 
     @DBRef(lazy = true)
     private List<Chat> Chats;
@@ -33,11 +31,11 @@ public class Channel {
         return this;
     }
 
-    public void addUser(User user) {
+    public void addUser(ChannelUser user) {
         this.users.add(user);
     }
 
-    public void deleteUser(User user) {
+    public void deleteUser(ChannelUser user) {
         this.users.remove(user);
     }
 

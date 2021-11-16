5abf4ddfaa27f17954f078c9700aec9a399919c8
임서영 <sylim0318@naver.com>
2021-11-16 13:54:21 +0900

    [ADD]  notice list

--- InTechs-backend/src/main/java/InTechs/InTechs/channel/controller/NoticeController.java
+++ InTechs-backend/src/main/java/InTechs/InTechs/channel/controller/NoticeController.java
@@ -1,9 +1,8 @@
-package InTechs.InTechs.chat.controller;
+package InTechs.InTechs.channel.controller;
 
-import InTechs.InTechs.chat.payload.request.NoticeRequest;
-import InTechs.InTechs.chat.payload.response.ChatResponse;
-import InTechs.InTechs.chat.payload.response.NoticeResponse;
-import InTechs.InTechs.chat.service.NoticeService;
+import InTechs.InTechs.channel.payload.request.NoticeRequest;
+import InTechs.InTechs.channel.payload.response.NoticeResponse;
+import InTechs.InTechs.channel.service.NoticeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,18 +15,18 @@ public class NoticeController {
 
     private final NoticeService noticeService;
 
-    @PatchMapping("/{chatId}")
+    @PatchMapping("/{chatId}/notice")
     public void setNotice(@PathVariable String chatId,
                           @RequestBody NoticeRequest noticeRequest) {
 
         noticeService.updateNotice(chatId, noticeRequest);
     }
-//
-//    @GetMapping("/{channelId}/notices")
-//    public List<ChatResponse> getNotices(@PathVariable String channelId) {
-//        return noticeService.noticeList(channelId);
-//    }
-//
+
+    @GetMapping("/{channelId}/notices")
+    public List<NoticeResponse> getNotices(@PathVariable String channelId) {
+        return noticeService.noticeList(channelId);
+    }
+
     @GetMapping("/{channelId}")
     public NoticeResponse currentNotice(@PathVariable String channelId) {
         return noticeService.currentNotice(channelId);

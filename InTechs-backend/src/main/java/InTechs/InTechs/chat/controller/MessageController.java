123f94d8584e5c71a300173bb315dcdbf7bcd7c5
joungeun <lje0818ovo@naver.com>
2021-11-20 16:46:31 +0900

    [ADD] @RequestMapping

--- InTechs-backend/src/main/java/InTechs/InTechs/chat/controller/MessageController.java
+++ InTechs-backend/src/main/java/InTechs/InTechs/chat/controller/MessageController.java
@@ -1,26 +1,31 @@
 package InTechs.InTechs.chat.controller;
 
+import InTechs.InTechs.chat.payload.response.ChatResponse;
 import InTechs.InTechs.chat.payload.response.ChatsResponse;
 import InTechs.InTechs.chat.service.MessageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RequiredArgsConstructor
+@RequestMapping("/channel/{channelId}/chat")
 @RestController
 public class MessageController {
     private final MessageService messageService;
 
-    @GetMapping("/channel/{channelId}/chat/{message}}")
-    public void messageSearch(@PathVariable String channelId, @PathVariable String message){
-        messageService.messageSearch(channelId, message);
-    }
-
-    @GetMapping("/{channelId}/chat")
+    @GetMapping
     public ChatsResponse readChannelChat(@PathVariable String channelId, final Pageable pageable){
         return messageService.readChat(channelId, pageable);
+    }
+
+    @GetMapping("/{message}")
+    public List<ChatResponse> messageSearch(@PathVariable String channelId, @PathVariable String message){
+        return messageService.messageSearch(channelId, message);
     }
 
 }

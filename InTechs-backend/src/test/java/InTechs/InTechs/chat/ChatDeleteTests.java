b23ddc3d5d9f69faa7d280761d8a68a85e755802
joungeun <lje0818ovo@naver.com>
2021-11-23 22:21:42 +0900

    [TEST] Chat delete test - 더티 채킹은 안 되는 걸로...

--- InTechs-backend/src/test/java/InTechs/InTechs/chat/ChatDeleteTests.java
+++ InTechs-backend/src/test/java/InTechs/InTechs/chat/ChatDeleteTests.java
@@ -1,19 +1,44 @@
 package InTechs.InTechs.chat;
 
-import InTechs.InTechs.channel.repository.ChannelRepository;
+import InTechs.InTechs.chat.entity.Chat;
+import InTechs.InTechs.chat.entity.Sender;
 import InTechs.InTechs.chat.repository.ChatRepository;
+import InTechs.InTechs.chat.service.MessageService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.*;
+
+import java.time.LocalDateTime;
+
 @SpringBootTest
 public class ChatDeleteTests {
     @Autowired
-    ChannelRepository channelRepository;
+    MessageService messageService;
     @Autowired
     ChatRepository chatRepository;
     @Test
     public void chatDelete(){
-        chatRepository.deleteById("6199005af01dd5754eb4ab7e");
+        messageService.messageDelete("6198ffcca3450e6e6a87eaaf");
+    }
+
+    @Test
+    public void dirtyCheckingTest(){
+        Chat chat = Chat.builder()
+                .message("테스트dd")
+                .time(LocalDateTime.now())
+                .sender(Sender.builder().email("tpsddej").name("Dddad").image("dfas").build())
+                .notice(false)
+                .channelId("619757baa528e1194ea7dbc6")
+                .isDeleted(false).build();
+
+        chatRepository.save(chat);
+
+        messageService.messageDelete(chat.getId().toString());
+
+        Chat c = chatRepository.findById(chat.getId().toString()).orElseThrow();
+        assertThat(c.isDeleted()).isEqualTo(true);
+
     }
 }

4600e96de3e3ffe86784d5e65aa546dd11958efd
joungeun <lje0818ovo@naver.com>
2021-11-24 12:13:50 +0900

    [ADD] NotificationOnAndOff method for NotificationController 😊 지민아 고마워 ㅎㅎ

---
+++ InTechs-backend/src/main/java/InTechs/InTechs/notification/controller/NotificationController.java
@@ -0,0 +1,19 @@
+package InTechs.InTechs.notification.controller;
+
+import InTechs.InTechs.notification.service.NotificationService;
+import InTechs.InTechs.security.auth.AuthenticationFacade;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequiredArgsConstructor
+@RequestMapping("/channel/{channelId}/notification")
+public class NotificationController {
+    private final NotificationService notificationService;
+    private final AuthenticationFacade authenticationFacade;
+    @PatchMapping
+    public void notificationOnAndOff(@PathVariable String channelId){
+        notificationService.notificationStateChange(authenticationFacade.getUserEmail());
+    }
+}

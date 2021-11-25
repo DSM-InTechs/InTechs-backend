eb345a4f4a5b9aadc76d3e1867d6b75ab91f5cc8
임서영 <sylim0318@naver.com>
2021-11-25 22:06:20 +0900

    [UPDATE] channel userlist add

--- InTechs-backend/src/main/java/InTechs/InTechs/channel/service/ChannelService.java
+++ InTechs-backend/src/main/java/InTechs/InTechs/channel/service/ChannelService.java
@@ -1,13 +1,15 @@
 package InTechs.InTechs.channel.service;
 
 import InTechs.InTechs.channel.payload.request.ChannelRequest;
+import InTechs.InTechs.channel.payload.response.ChannelResponse;
 import InTechs.InTechs.user.payload.response.ProfileResponse;
 
+import java.io.IOException;
 import java.util.List;
 
 public interface ChannelService {
 
-    void updateChannel(String channelId, ChannelRequest channelRequest);
+    void updateChannel(String channelId, ChannelRequest channelRequest) throws IOException;
 
     void createChannel(int projectId, ChannelRequest channelRequest);
 
@@ -18,5 +20,7 @@ public interface ChannelService {
     void AddUser(String targetEmail, String channelId);
 
     void exitChannel(String channelId);
+
+    List<ChannelResponse> getChannels();
 
 }

92639260b6c6671e0c557ea742bfb0f2627d3642
임서영 <sylim0318@naver.com>
2021-12-01 22:03:15 +0900

    [UPDATE] 현재 공지 삭제

--- InTechs-backend/src/main/java/InTechs/InTechs/channel/service/NoticeService.java
+++ InTechs-backend/src/main/java/InTechs/InTechs/channel/service/NoticeService.java
@@ -9,8 +9,6 @@ public interface NoticeService {
 
     void updateNotice(int projectId, String channelId, String chatId, NoticeRequest noticeRequest);
 
-    NoticeResponse currentNotice(int projectId, String channelId);
-
     List<NoticeResponse> noticeList(int projectId, String channelId);
 
 }

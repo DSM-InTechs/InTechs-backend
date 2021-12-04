29ef68f235dd21ba955cd4ab102d7d98ffde0fae
joungeun <lje0818ovo@naver.com>
2021-12-04 16:15:26 +0900

    [ADD] chatId for ChatUpdateRequest

--- InTechs-backend/src/main/java/InTechs/InTechs/chat/payload/request/ChatUpdateRequest.java
+++ InTechs-backend/src/main/java/InTechs/InTechs/chat/payload/request/ChatUpdateRequest.java
@@ -1,6 +1,10 @@
 package InTechs.InTechs.chat.payload.request;
 
+import lombok.Getter;
+
+@Getter
 public class ChatUpdateRequest {
     private String channelId;
+    private String chatId;
     private String message;
 }

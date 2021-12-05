330629b93e64dfd8c4a56649b02cd34ed49092cb
joungeun <lje0818ovo@naver.com>
2021-12-05 12:59:35 +0900

    [FIX] ChatResponse에서 @JsonFormat 삭제

--- InTechs-backend/src/main/java/InTechs/InTechs/chat/payload/response/ChatResponse.java
+++ InTechs-backend/src/main/java/InTechs/InTechs/chat/payload/response/ChatResponse.java
@@ -2,25 +2,20 @@ package InTechs.InTechs.chat.payload.response;
 
 import InTechs.InTechs.chat.entity.ChatType;
 import InTechs.InTechs.chat.entity.Sender;
-import com.fasterxml.jackson.annotation.JsonFormat;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Getter
 @Builder
-@NoArgsConstructor
-@AllArgsConstructor
 public class ChatResponse {
-    private String id;
-    private String message;
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    private LocalDateTime time;
-    private Sender sender;
-    private Boolean isMine;
-    private boolean isDelete;
-    private ChatType chatType;
+    private final String id;
+    private final String message;
+//    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private final LocalDateTime time;
+    private final Sender sender;
+    private final Boolean isMine;
+    private final boolean isDelete;
+    private final ChatType chatType;
 }
